10a5aa94f6fe9f5f2aa3d6ea0539efdade2507e4
kartikey sharma <kartikey7518@gmail.com>
2025-09-03 16:29:32 +0530

    user profile clerk

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,7 +11,9 @@ if(!PUBLISHABLE_KEY){
 }
 
 createRoot(document.getElementById('root')!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}
+  afterSignOutUrl="/"
+  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
