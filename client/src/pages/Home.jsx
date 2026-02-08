a0a5983fcb62e5f6f7a672dc7e55a75506afe5ba
kartikey sharma <kartikey7518@gmail.com>
2026-02-09 05:10:53 +0530

    created testimonial section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import ExclusiveOffers from "../components/ExclusiveOffers"
 import FeaturedDestination from "../components/FeaturedDestination"
 import Hero from "../components/Hero"
+import Testimonials from "../components/Testimonials"
 
 const Home = () => {
   return (
     <>
         <Hero />
         <FeaturedDestination />
+        <ExclusiveOffers />
+        <Testimonials />
     </>
   )
 }
