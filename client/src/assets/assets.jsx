50cc169b80ccd2beada2106d469a821d208d26f1
kartikey sharma <kartikey7518@gmail.com>
2026-02-09 01:02:02 +0530

    added authentication using clerk

--- client/src/assets/assets.jsx
+++ client/src/assets/assets.jsx
@@ -1,4 +1,4 @@
-import logo from './logo123.png'
+import logo from './logo.png'
 import searchIcon from './searchIcon.svg'
 import userIcon from './userIcon.svg'
 import calenderIcon from './calenderIcon.svg'
