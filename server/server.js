0ea0601b58d7579462731236363dafbf1b9405b2
kartikey sharma <kartikey7518@gmail.com>
2026-02-25 02:32:31 +0530

    bootstrapped backend

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDB from "./configs/db.js";
+
+connectDB()
+
+const app = express();
+app.use(cors())
+
+app.get('/', (req, res)=> {
+    res.send("API working")
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT }`)
+})
